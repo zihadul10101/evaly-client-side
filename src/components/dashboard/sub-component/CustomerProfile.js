1464914acee476ec7d1050309ec5414fa37e16c3
Zihadul Islam <zihadul10101@gmail.com>
2021-12-13 23:13:32 +0600

    another updated

--- src/components/dashboard/sub-component/CustomerProfile.js
+++ src/components/dashboard/sub-component/CustomerProfile.js
@@ -11,10 +11,7 @@ const CustomerProfile = () => {
         <div>
             <div className="container d-flex justify-content-center align-items-center">
                 <div className="card">
-                    <div className="upper"> <img src="https://i.imgur.com/Qtrsrk5.jpg" className="img-fluid" /> </div>
-                    <div className="user text-center">
-                        <div className="profile"> <img src="https://i.imgur.com/JgYD2nQ.jpg" className="rounded-circle" width="80" /> </div>
-                    </div>
+                   
                     <div className="mt-5 text-center">
                         <h4 className="mb-0">Your Email</h4> <span className="text-muted d-block mb-2">{profile}</span>
 
