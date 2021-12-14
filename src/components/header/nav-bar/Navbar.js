8b12c53cdf8eb1f5108fe1ae8b4811a708b15a7c
Zihadul Islam <zihadul10101@gmail.com>
2021-12-14 18:39:49 +0600

    add new product

--- src/components/header/nav-bar/Navbar.js
+++ src/components/header/nav-bar/Navbar.js
@@ -5,7 +5,7 @@ import { logOutAction } from '../../../redux/action/action';
 
 const Navbar = () => {
     const dispatch = useDispatch();
-    const email = useSelector((state) => state?.auth?.authdetails?.email)
+    const logout = useSelector((state) => state?.auth?.authdetails?.email)
 
     return (
         <div  className="mt-5">
@@ -44,7 +44,7 @@ const Navbar = () => {
                         </li>
 
                         {
-                          email && <>
+                          logout && <>
                           
                           <li className="nav-item">
                             <Link onClick={() => dispatch(logOutAction())} to="/" className="nav-link" >LogOut</Link>
