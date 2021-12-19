d8959349e1416f7233cee262cf1614e386619f26
Zihadul Islam <zihadul10101@gmail.com>
2021-12-19 18:16:24 +0600

    done by single user list

--- src/components/dashboard/sub-component/UserOrderList.js
+++ src/components/dashboard/sub-component/UserOrderList.js
@@ -8,13 +8,14 @@ const UserOrderList = () => {
     const user = useSelector((state) => state?.auth?.authdetails)
     console.log(user);
     const userId = user._id;
-    const [userOrder, setUserOrder] = useState([]);
-    console.log(userOrder);
+    const [userOrder, setUserOrder] = useState([{}]);
+   console.log(userOrder);
+   
     const getAllOrders = async () => {
         try {
             const res = await axios.get(`http://localhost:5500/api/order/single/${userId}`);
             setUserOrder(res.data)
-            console.log(res);
+            // console.log(res.data[0]);
 
         } catch (error) {
             console.error(error);
@@ -27,8 +28,8 @@ const UserOrderList = () => {
     return (
         <div>
             <h3>Order List Comming Soon</h3>
-          
-
+        {userOrder[0].productName}
+      
         </div>
     );
 };
